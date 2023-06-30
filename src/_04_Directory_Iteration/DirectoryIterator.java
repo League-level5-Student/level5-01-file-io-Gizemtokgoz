3a398d573c5b1dab62da21d4bce84a7a83b36f1b
Student <league@iMac-20.attlocal.net>
2023-06-29 18:17:15 -0700

    DirectoryIterator IP

--- src/_04_Directory_Iteration/DirectoryIterator.java
+++ src/_04_Directory_Iteration/DirectoryIterator.java
@@ -1,12 +1,15 @@
 package _04_Directory_Iteration;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 
 public class DirectoryIterator {
 	public static void main(String[] args) {
-		/* 
+		/*
 		 * The following is an example of how to list all of the files in a directory.
 		 * Once the program is running, the directory is chosen using the JFileChooser.
 		 */
@@ -16,18 +19,29 @@ public class DirectoryIterator {
 		if (returnVal == JFileChooser.APPROVE_OPTION) {
 			File directory = jfc.getSelectedFile();
 			File[] files = directory.listFiles();
-			if(files != null) {
-				for(File f : files) {
-				  System.out.println(f.getAbsolutePath());
+			if (files != null) {
+				for (File f : files) {
+					if (f.isFile() == true && f.getName().contains(".java")) {
+						try {
+							FileWriter fw = new FileWriter("src/FileRecorder", true);
+							String input = "Copyright © 2019 Gizem Tokgoz";
+							fw.write(input);
+							fw.close();
+						} catch (IOException e) {
+							e.printStackTrace();
+						}
+					}
 				}
 			}
+
 		}
-		
+
 		/*
-		 * Your task is to write a program that iterates through the src folder of this current Java Project. 
-		 * For every .java file it finds, the program will add a (non-legally binding) copyright statement at the bottom.
-		 * Be aware of possible directories inside of directories.
-		 * (e.g //Copyright © 2019 FirstName LastName)
+		 * Your task is to write a program that iterates through the src folder of this
+		 * current Java Project. For every .java file it finds, the program will add a
+		 * (non-legally binding) copyright statement at the bottom. Be aware of possible
+		 * directories inside of directories. (e.g //Copyright © 2019 FirstName
+		 * LastName)
 		 */
 	}
 }
